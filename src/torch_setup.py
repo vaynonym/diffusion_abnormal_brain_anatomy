f8f410c4a66226f3b6c55190d85458964e367c02
Vaynonym <timruschke@googlemail.com>
2024-04-08 15:29:57 +0200

    show visible cuda devices

--- src/torch_setup.py
+++ src/torch_setup.py
@@ -1,5 +1,7 @@
 import torch
 from src.logging_util import LOGGER
+import os
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-LOGGER.info(f"Using {torch.cuda.get_device_name(device) if torch.cuda.is_available() else 'cpu'}")
+LOGGER.info(f"Using device {os.environ.get('CUDA_VISIBLE_DEVICES')} of {torch.cuda.get_device_name(device)}"
+              if torch.cuda.is_available() else "cpu")
