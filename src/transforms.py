6f2c8844c99f1d0332f35a2931584619b9ba9da0
Vaynonym <timruschke@googlemail.com>
2024-04-30 15:19:16 +0200

    mask-mased center-crop used crop_size directly instead of going by run_config proxy

--- src/transforms.py
+++ src/transforms.py
@@ -1,6 +1,6 @@
 from monai import transforms
 
-def get_crop_around_mask_center(run_config):
+def get_crop_around_mask_center(crop_size):
     def crop_around_mask_center(d):
         is_not_background = (d["mask"] > 0.0).nonzero()
         max_indices, _ = is_not_background[:, 1:].max(dim=0)
@@ -8,8 +8,8 @@ def get_crop_around_mask_center(run_config):
 
         center = min_indices + (max_indices - min_indices) // 2
 
-        crop = transforms.SpatialCrop(roi_center=center, roi_size=run_config["input_image_crop_roi"])
-        pad = transforms.SpatialPad(spatial_size=run_config["input_image_crop_roi"], value=0)
+        crop = transforms.SpatialCrop(roi_center=center, roi_size=crop_size)
+        pad = transforms.SpatialPad(spatial_size=crop_size, value=0)
 
         return {
             **d,
