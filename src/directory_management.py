5b3ef79de42bf387ecd9b0a0c6b41959427f794a
Vaynonym <timruschke@googlemail.com>
2024-04-30 15:15:44 +0200

    Remove doubled DATA_DIRECTORY check, add FLAIR_DATASET_DIRECTORY environment variable

--- src/directory_management.py
+++ src/directory_management.py
@@ -2,15 +2,18 @@ import os
 
 PRETRAINED_MODEL_DIRECTORY = os.environ.get("PRETRAINED_MODEL_DIRECTORY")
 DATA_DIRECTORY = os.environ.get("DATA_DIRECTORY")
+FLAIR_DATASET_DIRECTORY = os.environ.get("FLAIR_DATASET_DIRECTORY")
 BASE_DIRECTORY = os.environ.get("BASE_DIRECTORY")
 OUTPUT_DIRECTORY = os.environ.get("OUTPUT_DIRECTORY")
 MODEL_DIRECTORY = os.environ.get("MODEL_DIRECTORY")
 
 for dir_path in [PRETRAINED_MODEL_DIRECTORY,
-                 DATA_DIRECTORY,
-                 DATA_DIRECTORY,
                  OUTPUT_DIRECTORY,
+                 DATA_DIRECTORY,
+                 FLAIR_DATASET_DIRECTORY,
                  MODEL_DIRECTORY,
                  ]:
     assert os.path.exists(dir_path), f"path {dir_path} needs to exist"
     assert os.path.isdir(dir_path), f"path {dir_path} needs to be a directory"
+
+
