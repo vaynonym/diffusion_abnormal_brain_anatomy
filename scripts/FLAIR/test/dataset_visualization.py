86b078b423779c53045f315e4ddb7be163b11f6c
Vaynonym <timruschke@googlemail.com>
2024-05-29 18:49:56 +0200

    Visualization script

---
+++ scripts/FLAIR/test/dataset_visualization.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+# Set up imports
+import os
+import torch
+import torch.nn.functional as F
+import torch.multiprocessing
+import wandb
+from src.torch_setup import device
+
+from monai import transforms
+from monai.utils import set_determinism
+
+
+from src.util import Stopwatch, read_config
+from src.logging_util import LOGGER
+from src.datasets import get_dataloader
+
+
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+LOGGER.info(f"Device count: {torch.cuda.device_count()}", )
+LOGGER.info(f"Device: {device}")
+
+
+WANDB_LOG_IMAGES = os.environ.get("WANDB_LOG_IMAGES")
+WANDB_RUN_NAME = os.environ.get("WANDB_RUN_NAME")
+
+experiment_config = read_config(os.environ.get("EXPERIMENT_CONFIG"))
+run_config = experiment_config["run"]
+
+# for reproducibility purposes set a seed
+set_determinism(42)
+
+import numpy as np
+
+
+from src.synthseg_masks import decode_one_hot, encode_contiguous_labels_one_hot
+import math
+
+from src.synthseg_masks import synthseg_class_to_string_map, ventricles
+from collections import defaultdict
+from src.util import visualize_3d_image_slice_wise
+from src.synthseg_masks import encode_one_hot, decode_one_hot_to_consecutive_indices
+from tqdm import tqdm
+from monai.data.meta_tensor import MetaTensor
+from pprint import pprint
+
+prediction_models = {
+ "NNU Real": "777",
+ "NNU Augmented": "778",
+ "NNU Synthetic": "801"
+}
+
+dataset_paths = {
+    "Real" : "Dataset777_RealBrainFlair",
+    "Augmented" : "Dataset778_AugmentedBrainFlair",
+    "Synthetic" : "Dataset801_PurelySynthetic",
+}
+
+base_path = "/depict/users/tim/private/correct_masks/nnUNet_raw/"
+
+label_path = "labelsTr"
+
+def synthseg_to_binary_ventricle_mask(mask):
+    ventricle_indices = torch.tensor([index for index, name in synthseg_class_to_string_map.items() 
+                                      if name in ventricles]).to(device)
+
+    return torch.isin(mask, ventricle_indices)
+
+def ventricle_only_mask_to_binary_ventricle_mask(mask):
+    return mask > 0
+
+def get_volumes_for_dataset(dataset_path):
+    full_label_path = os.path.join(base_path, dataset_path, label_path)
+    label_file_names = [x for x in os.listdir(full_label_path) if x.endswith(".nii.gz") ]
+
+    MM_3_TO_MLL_CONSTANT = 0.001
+    SPACING_CONSTANT = 1.75 * 1.667 * 1.667
+
+    all_volumes = []
+    import random
+    random.shuffle(label_file_names)
+
+    for label_file_name in tqdm(label_file_names[:1000]):
+
+        mask = transforms.LoadImaged(keys="mask")({"mask": os.path.join(full_label_path, label_file_name)})["mask"].int().unsqueeze(0).unsqueeze(0)
+        mask = synthseg_to_binary_ventricle_mask(decode_one_hot(encode_contiguous_labels_one_hot(mask.to(device)))).cpu().detach()
+
+        volume_in_MLL = mask.sum().item() * SPACING_CONSTANT * MM_3_TO_MLL_CONSTANT
+        all_volumes.append(volume_in_MLL)
+    
+    return all_volumes
+
+results = {}
+
+for name, dataset_path in dataset_paths.items():
+    volumes_for_dataset = get_volumes_for_dataset(dataset_path)
+    volumes_for_dataset.sort()
+    results[name] = volumes_for_dataset
+
+
+import matplotlib.pyplot as plt
+
+colors = ["orange", "green", "purple"]
+
+all_volumes = list(results.values())
+labels = list(results.keys())
+
+plt.stackplot(range(len(all_volumes[0])), *all_volumes, labels=labels, colors=colors, alpha=0.6)
+
+lgd = plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+plt.xticks([])
+plt.xlabel("Patients")
+plt.ylabel("Ventricular Volume in ml")
+plt.savefig("../output/volumes_stackplot.png", bbox_extra_artists=[lgd], bbox_inches="tight")
