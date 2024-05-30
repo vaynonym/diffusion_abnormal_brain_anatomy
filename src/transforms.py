e282fac4d7faf40cef23d900387d8941f33846f6
Vaynonym <timruschke@googlemail.com>
2024-05-30 22:52:11 +0200

    center crop more robust and handles synthseg_mask if available

--- src/transforms.py
+++ src/transforms.py
@@ -3,6 +3,9 @@ from monai import transforms
 def get_crop_around_mask_center(crop_size):
     def crop_around_mask_center(d):
         is_not_background = (d["mask"] > 0.0).nonzero()
+        if is_not_background.shape[0] == 0:
+            raise Exception(f"Mask is Empty: {d['case_identifier']}")
+        
         max_indices, _ = is_not_background[:, 1:].max(dim=0)
         min_indices, _= is_not_background[:, 1:].min(dim=0)
 
@@ -11,9 +14,17 @@ def get_crop_around_mask_center(crop_size):
         crop = transforms.SpatialCrop(roi_center=center, roi_size=crop_size)
         pad = transforms.SpatialPad(spatial_size=crop_size, value=0)
 
-        return {
+
+        result = {
             **d,
             "image": pad(crop(d["image"])),
             "mask": pad(crop(d["mask"])),
         }
+
+        if "synthseg_mask" in d:
+            result = {**result, "synthseg_mask": pad(crop(d["synthseg_mask"]))}
+        
+        return result
+
+
     return crop_around_mask_center
